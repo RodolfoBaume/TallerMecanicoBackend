e75495b34f2f2a848be90450c8da656ee0bde304
RodolfoBaume <robalaz@hotmail.com>
2024-04-03 00:19:50 -0600

    Detalle de Orden de servicio completo

--- src/main/java/com/tallerMecanico/entity/DetalleOrdenServicio.java
+++ src/main/java/com/tallerMecanico/entity/DetalleOrdenServicio.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -22,8 +23,9 @@ import lombok.ToString;
 public class DetalleOrdenServicio {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long idOrdenServicios;
-	@ManyToOne
-	private OrdenServicio ordenServicio;
+	private long idDetalleOrdenServicio;
 	private String descripcionServicio;
+	@ManyToOne
+	@JoinColumn(name = "ordenServicioId")
+	private OrdenServicio ordenServicio;
 }
